d5ba95e5ecd5af038e61140e96ea44bdc3ca63c2
sayiO <sayionias@gmail.com>
2021-01-13 03:41:00 +0200

    made some deletions

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-ECHO is on.
+let count = 0;
+
+const value = document.querySelector('#value');
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const increase = document.querySelector(".increase");
+
+increase.addEventListener("click", function(e){
+    const actions = e.currentTarget.classList;
+    if(actions.contains('increase')){
+        count++;
+    }
+    value.textContent = count;
+})
+
+reset.addEventListener("click", function(e){
+    const actions = e.currentTarget.classList;
+    if(actions.contains('reset')){
+        count = 0;
+    }
+    value.textContent = count;
+})
+
+decrease.addEventListener("click", function(e){
+    const actions = e.currentTarget.classList;
+    if(actions.contains("decrease")){
+        count--;
+    }
+    value.textContent = count;
+})
